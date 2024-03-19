f3c6ecae466ea0b9f402fc17b6cb8d6fa5d5651e
LIJINLIN-max <jl288@rice.edu>
2024-03-18 20:06:34 -0400

    add Implementation 3

--- hexdump.cpp
+++ hexdump.cpp
@@ -7,12 +7,16 @@
 #include <vector>
 #include <random>
 #include <set>
+#include <fstream>
+#include <cxxopts.hpp>
+#include <fstream>
 
-template <class Range> requires std::ranges::range<Range>
+template <class Range>
+    requires std::ranges::range<Range>
 void hexdump(Range const &range, uint32_t width)
 {
     uint32_t addr = 0;
-
+    std::vector<char> saved;
     for (auto chunk : range | std::views::chunk(width))
     {
         using T = std::ranges::range_value_t<Range>;
@@ -21,40 +25,80 @@ void hexdump(Range const &range, uint32_t width)
         {
             std::cout << ' ' << std::right << std::hex << std::setw(2) << std::setfill('0') << (unsigned long long)(std::make_unsigned_t<T>)c;
             ++addr;
-        }
-        if constexpr (sizeof(T) == 1 && std::convertible_to<T, char>)
-        {
-            if (addr % width < width)
+            if constexpr (sizeof(T) == sizeof(char) && std::convertible_to<T, char>)
             {
-                for (size_t i = 0; i < (width - addr % width) * 3; ++i)
+                saved.push_back(static_cast<char>(c));
+            }
+        }
+        if constexpr (sizeof(T) == sizeof(char) && std::convertible_to<T, char>)
+        {
+            if (addr % width != 0)
+            {
+                for (std::size_t i = 0; i < (width - addr % width) * 3; i++)
                 {
                     std::cout << ' ';
                 }
             }
-            std::cout << "   |";
-            for (auto c0 : chunk)
+            std::cout << "  |";
+            for (auto c : saved)
             {
-                char c = static_cast<char>(c0);
                 if (!std::isprint(c))
                 {
                     c = '.';
                 }
                 std::cout << c;
             }
-            std::cout << "| \n";
+            std::cout << "|";
+            saved.clear();
         }
+        std::cout << '\n';
     }
 }
 
-int main(int argc, char const *argv[])
+struct IstreamRange
 {
-    std::set<uint8_t> s;
-    std::mt19937 gen(3);
-    std::uniform_int_distribution<uint16_t> dis;
-    for (int i = 0; i < 100; ++i)
+    std::istreambuf_iterator<char> b, e;
+
+    IstreamRange(std::istream &is)
+        : b(std::istreambuf_iterator<char>(is)), e(std::istreambuf_iterator<char>()) {}
+
+    auto begin() const
     {
-        s.insert(dis(gen));
+        return b;
     }
-    hexdump(s, 16);
+
+    auto end() const
+    {
+        return e;
+    }
+};
+
+int main(int argc, char **argv)
+{
+    cxxopts::Options options("hexdump", "A command line utility for printing the hexadecimal and ASCII representation of a file.");
+    options.add_options()("f,file", "Input file", cxxopts::value<std::string>()->default_value("-"));
+    options.add_options()("w,width", "Number of bytes per line", cxxopts::value<std::size_t>()->default_value("16"));
+    // add help
+    options.add_options()("h,help", "Print usage");
+    auto args = options.parse(argc, argv);
+    if (args.count("help"))
+    {
+        std::cout << options.help() << '\n';
+        return 0;
+    }
+    auto path = args["file"].as<std::string>();
+    auto width = args["width"].as<std::size_t>();
+    if (path == "-")
+    {
+        hexdump(IstreamRange(std::cin), width);
+        return 0;
+    }
+    std::ifstream ifs(path);
+    if (!ifs.good())
+    {
+        std::cerr << std::strerror(errno) << ": " << path << '\n';
+        return errno;
+    }
+    hexdump(IstreamRange(ifs), width);
     return 0;
 }
